493830618cc77978522668ae145088400c1a4dab
ldlharper <ldlharper@gmail.com>
2026-02-27 14:26:14 +0000

    CDIA-10 Fix copy paste mistake. (#15)
    Co-authored-by: Luke Harper <luke.harper4@justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/config/ResourceServerConfiguration.kt
@@ -1,4 +1,4 @@
-package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.config
+package uk.gov.justice.digital.hmpps.courtdataingestionapi.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
