91df276bf18901821f8d197c6b75912e69a0c7ad
Joel Stobart <>
2026-05-21 14:01:51 +0100

    Switch to a kubernetes model for direct calling of the extraction backfill

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/controller/ExtractionAdminController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/controller/ExtractionAdminController.kt
@@ -1,28 +1,19 @@
 package uk.gov.justice.digital.hmpps.courtdataingestionapi.controller
 
-import io.swagger.v3.oas.annotations.Operation
-import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation
 import org.springframework.core.task.TaskExecutor
-import org.springframework.http.HttpStatus
-import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.service.extraction.ExtractionBackfillService
 
-@RestController
-@RequestMapping("/maintenance/extraction")
-class ExtractionAdminController(
+@Component
+@Endpoint(id = "extraction-backfill")
+class ExtractionBackfillEndpoint(
   private val backfillService: ExtractionBackfillService,
-  @Qualifier("applicationTaskExecutor")
-  private val taskExecutor: TaskExecutor,
 ) {
-  @PostMapping("/backfill")
-  @ResponseStatus(HttpStatus.ACCEPTED)
-  @PreAuthorize("hasRole('ROLE_COURT_DATA_ADMIN')")
-  @Operation(summary = "Extract or re-extract all stored documents missing a result at the active versions")
-  fun startBackfill() {
-    taskExecutor.execute { backfillService.runBackfill() }
+  @WriteOperation
+  fun trigger(): Map<String, String> {
+    val started = backfillService.runBackfill()
+    return mapOf("status" to if (started) "complete" else "already-running")
   }
 }
