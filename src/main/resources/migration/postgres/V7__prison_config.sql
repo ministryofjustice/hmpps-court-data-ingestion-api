208a0f30fd2e6f3e0142618b1bba2d6e6f1c234b
Tomas Novellino <tomas.novellino1@justice.gov.uk>
2026-05-27 13:20:03 +0100

    CDIA-85: renamed table and column to make it more specific

--- src/main/resources/migration/postgres/V7__prison_config.sql
+++ src/main/resources/migration/postgres/V7__prison_config.sql
@@ -1,5 +1,5 @@
-CREATE TABLE prison_config
+CREATE TABLE prison_doc_notification_config
 (
-    prison_id                           VARCHAR(6)                  NOT NULL CONSTRAINT prison_config_pk PRIMARY KEY,
-    new_doc_notification_date_from      TIMESTAMP WITH TIME ZONE    NOT NULL
+    prison_id              VARCHAR(6)                  NOT NULL CONSTRAINT prison_config_pk PRIMARY KEY,
+    new_doc_date_from      TIMESTAMP WITH TIME ZONE    NOT NULL
 );
