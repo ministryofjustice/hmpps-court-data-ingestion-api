d7ac7f0c3772e974dc4b7bd50b4cbb2a6fb2b01a
Tomas Novellino <tomas.novellino1@justice.gov.uk>
2026-06-04 10:59:31 +0100

    CDIA-118: removed unused import

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/service/ThingsToDoService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/service/ThingsToDoService.kt
@@ -2,7 +2,6 @@ package uk.gov.justice.digital.hmpps.courtdataingestionapi.service
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import uk.gov.justice.digital.hmpps.courtdataingestionapi.entity.CourtDocumentEntity
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.model.api.ThingsToDo
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.model.api.ToDoType
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.repository.CourtDocumentRepository
