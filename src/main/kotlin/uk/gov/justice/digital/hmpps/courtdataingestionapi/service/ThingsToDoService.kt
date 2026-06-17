c476b6096d68d654dc3ebaad6d670e7578028e14
Tomas Novellino <tomas.novellino1@justice.gov.uk>
2026-06-17 22:29:06 +0100

    CDIA-132: Refactored service

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/service/ThingsToDoService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/service/ThingsToDoService.kt
@@ -5,6 +5,7 @@ import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.entity.CourtDocumentEntity
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.model.api.ThingsToDo
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.model.api.ToDoType
+import uk.gov.justice.digital.hmpps.courtdataingestionapi.model.documents.Document
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.model.documents.DocumentSearchByUuidsRequest
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.repository.CourtDocumentRepository
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.service.document.DocumentSearchService
@@ -19,19 +20,16 @@ class ThingsToDoService(
 ) {
 
   fun getToDoList(prisonerId: String): ThingsToDo {
-    val courtDocuments: List<CourtDocumentEntity> = courtDocumentRepository.findByPrisonerNumber(prisonerId)
+    val unreadCourtDocuments: List<CourtDocumentEntity> = courtDocumentRepository.findByPrisonerNumber(prisonerId)
       .filter { documentNotificationService.isUnread(it) }
 
-    val unreadDocumentUuids: List<UUID> = courtDocuments.map { it.prisonDocumentId }
-    val documents = documentSearchService.search(DocumentSearchByUuidsRequest(unreadDocumentUuids))
-    val nonDuplicateDocumentUuids: List<UUID> = documents.filter { it.duplicateOf == null }
-      .map { it.documentUuid }
+    val unreadDocumentUuids: List<UUID> = unreadCourtDocuments.map { it.prisonDocumentId }
+    val unreadDocuments = documentSearchService.search(DocumentSearchByUuidsRequest(unreadDocumentUuids))
+    val unreadNonDuplicateDocuments: List<Document> = unreadDocuments.filter { it.duplicateOf == null && unreadDocumentUuids.contains(it.documentUuid) }
 
     return ThingsToDo(
       prisonerId = prisonerId,
-      thingsToDo = unreadDocumentUuids.filter {
-        nonDuplicateDocumentUuids.contains(it)
-      }.map {
+      thingsToDo = unreadNonDuplicateDocuments.map {
         ToDoType.HMCTS_API_DOCUMENT_RECEIVED
       },
     )
