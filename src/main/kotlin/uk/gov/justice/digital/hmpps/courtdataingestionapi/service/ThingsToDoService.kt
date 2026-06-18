817c0c3883983a943b78ee08857da2d440b9c291
Tomas Novellino <tomas.novellino1@justice.gov.uk>
2026-06-18 14:36:06 +0100

    CDIA-132: Refactored after code review

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/service/ThingsToDoService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/service/ThingsToDoService.kt
@@ -2,30 +2,28 @@ package uk.gov.justice.digital.hmpps.courtdataingestionapi.service
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.courtdataingestionapi.client.HmppsDocumentManagementApi
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.entity.CourtDocumentEntity
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.model.api.ThingsToDo
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.model.api.ToDoType
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.model.documents.Document
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.model.documents.DocumentSearchByUuidsRequest
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.repository.CourtDocumentRepository
-import uk.gov.justice.digital.hmpps.courtdataingestionapi.service.document.DocumentSearchService
 import java.util.UUID
 
 @Service
 @Transactional(readOnly = true)
 class ThingsToDoService(
   private val courtDocumentRepository: CourtDocumentRepository,
-  private val documentSearchService: DocumentSearchService,
   private val documentNotificationService: PrisonDocumentNotificationService,
+  private val documentManagementApiClient: HmppsDocumentManagementApi,
 ) {
 
   fun getToDoList(prisonerId: String): ThingsToDo {
     val unreadCourtDocuments: List<CourtDocumentEntity> = courtDocumentRepository.findByPrisonerNumber(prisonerId)
       .filter { documentNotificationService.isUnread(it) }
 
-    val unreadDocumentUuids: List<UUID> = unreadCourtDocuments.map { it.prisonDocumentId }
-    val unreadDocuments = documentSearchService.search(DocumentSearchByUuidsRequest(unreadDocumentUuids))
-    val unreadNonDuplicateDocuments: List<Document> = unreadDocuments.filter { it.duplicateOf == null && unreadDocumentUuids.contains(it.documentUuid) }
+    val unreadNonDuplicateDocuments: List<Document> = getNonDuplicateDocuments(unreadCourtDocuments)
 
     return ThingsToDo(
       prisonerId = prisonerId,
@@ -33,5 +31,13 @@ class ThingsToDoService(
         ToDoType.HMCTS_API_DOCUMENT_RECEIVED
       },
     )
+  }
+
+  private fun getNonDuplicateDocuments(courtDocuments: List<CourtDocumentEntity>): List<Document> {
+    if (courtDocuments.isEmpty()) return emptyList()
+
+    val courtDocumentUuids: List<UUID> = courtDocuments.map { it.prisonDocumentId }
+    val documents = documentManagementApiClient.searchByDocumentUuids(DocumentSearchByUuidsRequest(courtDocumentUuids))
+    return documents.filter { it.duplicateOf == null && courtDocumentUuids.contains(it.documentUuid) }
   }
 }
