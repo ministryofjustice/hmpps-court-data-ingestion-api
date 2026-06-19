528b48634c509a6083d334a238002dcc46344103
Tomas Novellino <tomas.novellino1@justice.gov.uk>
2026-06-19 12:53:38 +0100

    CDIA-132: Refactored to remove DocumentFindByUuidsRequest in favour of a simple collection of UUIDs as in new version on DMA

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/model/documents/DocumentFindByUuidsRequest.kt
+++
@@ -1,7 +0,0 @@
-package uk.gov.justice.digital.hmpps.courtdataingestionapi.model.documents
-
-import java.util.UUID
-
-data class DocumentFindByUuidsRequest(
-  val documentUuids: Collection<UUID>,
-)
