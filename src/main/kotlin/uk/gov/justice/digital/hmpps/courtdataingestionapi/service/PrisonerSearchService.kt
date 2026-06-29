1141018841d8dd060e33b648cade6e4d7d4ce1d9
Tomas Novellino <tomas.novellino1@justice.gov.uk>
2026-06-29 14:13:17 +0100

    CDIA-119: hotfix test to handle prisoner not found responses from search prisoner API

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/service/PrisonerSearchService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/service/PrisonerSearchService.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.courtdataingestionapi.service
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.client.PrisonerSearchApiClient
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.model.prisonersearch.Prisoner
 
@@ -14,8 +15,13 @@ class PrisonerSearchService(
 
   fun getPrison(prisonerId: String): String? {
     log.debug("Looking up prisoner for {}", prisonerId)
-    val prisoner: Prisoner = prisonerSearchApiClient.getByPrisonerNumber(prisonerId)
-    return prisoner.prisonId
+    try {
+      val prisoner: Prisoner = prisonerSearchApiClient.getByPrisonerNumber(prisonerId)
+      return prisoner.prisonId
+    } catch (e: WebClientResponseException.NotFound) {
+      log.error("Prisoner with ID {} not found, error {}", prisonerId, e.message)
+      return null
+    }
   }
 
   private companion object {
