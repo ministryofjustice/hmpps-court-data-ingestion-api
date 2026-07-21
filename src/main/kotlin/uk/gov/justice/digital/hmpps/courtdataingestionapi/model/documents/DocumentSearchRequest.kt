88285110eaca4d93ff9df4426947b8b295b0dd53
Tomas <tomas.novellino1@justice.gov.uk>
2026-07-21 13:30:48 +0100

    CDIA-174: CDIA document status Backfill (#177)
    * CDIA-238: Added new attribute and column court_code to the enrichment phase. Pending, lookup for its values on CourtRegistry API
    
    * CDIA-239: Added database comment on court_code column
    
    * CDIA-238: Added integration with court register API to collect courtCode for enrichment
    
    * CDIA-238: Added test coverage for courtCode integration via Court Register API
    
    * CDIA-238: Removed redundant commented code
    
    * CDIA-238: Updated ENV variables for integration with Court Register
    
    * CDIA-238: Updated court register configuration and environment variables, used hmpps court-cases-release-dates API as reference
    
    * CDIA-238: Updated metadata version up to 2, was 1
    
    * CDIA-238: Updated test for new version 2
    
    * CDIA-174: Added CDIA document status backfill
    
    * CDIA-174: Updated log

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/model/documents/DocumentSearchRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/model/documents/DocumentSearchRequest.kt
@@ -1,10 +1,12 @@
 package uk.gov.justice.digital.hmpps.courtdataingestionapi.model.documents
 
+import tools.jackson.databind.JsonNode
+
 data class DocumentSearchRequest(
 
   val documentTypes: List<DocumentApiType>?,
+  val metadata: JsonNode? = null,
 
-  // val metadata: JsonNode? TODO is this needed?
   val page: Int = 0,
   val pageSize: Int = 10,
 )
