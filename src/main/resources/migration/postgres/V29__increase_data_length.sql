3945204c6101ebb14879d316c1eca0fac3de421c
ldlharper <ldlharper@gmail.com>
2026-09-04 08:54:06 +0100

    CDIA-315 Increase length of fields. (#214)

---
+++ src/main/resources/migration/postgres/V29__increase_data_length.sql
@@ -0,0 +1,4 @@
+ALTER TABLE court_charge ALTER COLUMN offence_legislation TYPE text;
+ALTER TABLE court_charge ALTER COLUMN wording TYPE text;
+ALTER TABLE court_charge ALTER COLUMN title TYPE text;
+ALTER TABLE court_charge_result ALTER COLUMN result_description TYPE text;
