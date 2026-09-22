8686c6c7449f7094e0a7192673d7ceef03485c6f
Tomas Novellino <tomas.novellino1@justice.gov.uk>
2026-09-22 11:05:08 +0100

    CDIA-337: First draft of PACT tests for core-person-api

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/client/CorePersonApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtdataingestionapi/client/CorePersonApiClient.kt
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
 import uk.gov.justice.digital.hmpps.courtdataingestionapi.model.coreperson.CorePersonCanonicalRecord
 import java.util.UUID
 
@@ -17,12 +18,12 @@ import java.util.UUID
 class CorePersonApiClient(@Qualifier("corePersonApiWebClient") private val webClient: WebClient) : CorePersonProvider {
 
   override fun getPersonByCommonPlatformId(defendantId: UUID): CorePersonCanonicalRecord {
-    log.info("Getting core person record record for $defendantId")
+    log.info("Getting core person record for defendant $defendantId")
     return webClient
       .get()
       .uri("/person/commonplatform/$defendantId")
       .retrieve()
-      .bodyToMono(CorePersonCanonicalRecord::class.java)
+      .bodyToMono<CorePersonCanonicalRecord>()
       .block()!!
   }
 
@@ -30,12 +31,12 @@ class CorePersonApiClient(@Qualifier("corePersonApiWebClient") private val webCl
    * Can return a null response if the person has been merged, resulting in a 301. However, this is unlikely when using a prisoner created event.
    */
   override fun getPersonByPrisonerNumber(prisonerNumber: String): CorePersonCanonicalRecord? {
-    log.info("Getting core person record record for $prisonerNumber")
+    log.info("Getting core person record for prisoner $prisonerNumber")
     return webClient
       .get()
       .uri("/person/prison/$prisonerNumber")
       .retrieve()
-      .bodyToMono(CorePersonCanonicalRecord::class.java)
+      .bodyToMono<CorePersonCanonicalRecord>()
       .block()
   }
 
